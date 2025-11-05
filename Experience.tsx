136f942de6ea55bb8da6591222afb2ac2ef5ffc7
biruk habte <biruk.habte-ug@aau.edu.et>
2025-11-05 17:00:24 +0300

    Implement sticky navigation on scroll

--- Experience.tsx
+++ Experience.tsx
@@ -122,3 +122,4 @@
 2025-11-22T17:55:59 - Update content and project descriptions
 2025-11-13T16:00:33 - Implement responsive design for mobile devices
 2025-11-26T12:21:05 - Create 404 error page
+2025-11-05T17:00:24 - Implement sticky navigation on scroll
