65e3ec967eb034ff177f45447bfd76042cfcac56
biruk habte <biruk.habte-ug@aau.edu.et>
2025-11-08 11:52:33 +0300

    Create projects showcase section

--- Projects.tsx
+++ Projects.tsx
@@ -57,3 +57,4 @@
 2025-12-12T07:22:01 - Add skills section with technology icons
 2025-11-30T04:30:54 - Create component documentation
 2025-11-02T17:05:07 - Add breadcrumb navigation
+2025-11-08T11:52:33 - Create projects showcase section
