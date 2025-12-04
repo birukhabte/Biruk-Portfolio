49bdd9a6a2924c75ffd3c2ff9ec9dfcf803a2ff9
biruk habte <biruk.habte-ug@aau.edu.et>
2025-12-04 16:34:06 +0300

    Create projects showcase section

--- Navbar.tsx
+++ Navbar.tsx
@@ -45,3 +45,4 @@
 2025-12-26T05:28:27 - Create responsive navigation bar component
 2025-12-10T20:53:13 - Add Google Analytics integration
 2025-12-21T07:15:26 - Add meta tags for SEO optimization
+2025-12-04T16:34:06 - Create projects showcase section
